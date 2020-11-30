19dfbcf6d402f6ec41178698793cedd667fbb440
HazelEyes <67656747+iRCTgxV@users.noreply.github.com>
2020-11-30 12:20:32 +0100

    add db and imports

--- com.py
+++ com.py
@@ -1,5 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
+
+'''
+    Flask: web app
+    request: requests
+    jsonify: JSON output -> response obj with mime type of app
+    SQLalchemy: access db
+    marshmallow: serialize objects
+'''
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://<mysql_username>:<mysql_password>@<mysql_host>:<mysql_port>/<mysql_db>'
+db = SQLAlchemy(app)
+
 
 @app.route('/', methods=['POST'])
 
