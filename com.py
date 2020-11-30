2f09195a4a3ffc5207cbff31381a6b407168dda2
HazelEyes <67656747+iRCTgxV@users.noreply.github.com>
2020-11-30 14:50:42 +0100

    add db creds

--- com.py
+++ com.py
@@ -12,8 +12,8 @@ import projet_si
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_USER'] = 'phpmyadmin'
+app.config['MYSQL_PASSWORD'] = 'foo'
 app.config['MYSQL_DB'] = 'MyDB'
 
 mysql = MySQL(app)
