75a7d1ded228274560bb211924d87dd40b1396d9
Aveheuzed <a.masson555@ntymail.com>
2020-12-11 19:28:43 +0100

    Big API changes !
    -Message signatures now just consist in hash fingerprints (64 bytes)
    -Server no longer needs to be instantiated, its methods are classmethods
    -A "new_user" function has been moved from Server to the global scope
    -The underlying database structure has been modified; see Server

--- projet_si_poo.py
+++ projet_si_poo.py
@@ -19,28 +19,29 @@ class Message:
 
     def __setstate__(self, state) :
         self.sender, self.message, *parents = state
-        self.oldmessages = tuple(__class__.from_signature(**rpz) for rpz in parents)
+        self.oldmessages = tuple(__class__.from_signature(hsh) for hsh in parents)
 
-    def sign(self) :
+    def sign(self)->bytes :
         me = pickletools.optimize(pickle.dumps(self))
-        privkey = Server().get_privkey(self.sender)
+        privkey = Server.get_privkey(self.sender)
         hsh = rsa.compute_hash(me, __class__.HASHMETHOD)
         signature = rsa.sign_hash(hsh, privkey, __class__.HASHMETHOD)
-        Server().register_message(hsh)
-        return dict(plaintext=me, signature=signature, sender_id=self.sender)
+        Server.register_message(hsh, me, signature, self.sender)
+        return hsh
 
     @staticmethod
-    def from_signature(plaintext, signature, sender_id) :
-        """Raises rsa.VerificationError if the message can't be authenticated."""
-        # state is some yet-untrusted pickle dump; we can't load it until we've made sure it is safe
+    def from_signature(hsh:bytes) :
+        """Raises rsa.VerificationError if the message can't be authenticated.
+        Raises KeyError (from Server.get_message) if the message can't be found."""
+
+        message = Server.get_message(hsh)
+        sender_id = message["sender_id"]
+        signature = message["signature"]
+        plaintext = message["plaintext"]
+        # plaintext is some yet-untrusted pickle dump; we can't load it until we've made sure it is safe
         # in general, pickle may be used for code injection, see the pickle docs for details
 
-        hsh = rsa.compute_hash(plaintext, __class__.HASHMETHOD)
-
-        if not Server().check_existence(hsh) :
-            rsa.VerificationError(f"Message not found (hash : {hsh:8}...)")
-
-        pubkey = Server().get_pubkey(sender_id)
+        pubkey = Server.get_pubkey(sender_id)
 
         # may raise rsa.VerificationError
         hashfunc = rsa.verify(plaintext, signature, pubkey)
@@ -53,96 +54,70 @@ class Message:
 
 class Server:
 
-    __instance = None
+    __slots__ = ()
 
-    __userid = 0
+    __messages = dict()
+    __entities = dict()
 
-    __slots__ = ("pubkeys", "_privkeys", "hashes")
+    @classmethod
+    def get_pubkey(cls, user_id:int)->rsa.PublicKey :
+        return cls.__entities.get(user_id)["pubkey"]
 
-    def __new__(cls):
-        # singleton
-        if cls != __class__ :
-            return object.__new__(cls)
-        if cls.__instance is None :
-            instance = object.__new__(cls)
+    @classmethod
+    def get_privkey(cls, user_id:int)->rsa.PrivateKey :
+        return cls.__entities.get(user_id)["privkey"]
 
-            instance._privkeys = dict() # DEBUG only
+    @classmethod
+    def register_user(cls, nom:str, adresse:str, logo:str, *, pubkey:rsa.PublicKey, privkey:rsa.PrivateKey)->int :
+        uid = len(cls.__entities)
+        cls.__entities[uid] = dict(
+                                nom=nom,
+                                adresse=adresse,
+                                logo=logo,
+                                pubkey=pubkey,
+                                privkey=privkey
+                                )
+        return uid
 
-            instance.pubkeys = dict() # user_id : pubkey
-            instance.hashes = set() # plain hashes
+    @classmethod
+    def register_message(cls, hsh:bytes, plaintext:bytes, signature:bytes, sender_id:int)->None :
+        cls.__messages[hsh] = dict(
+                                plaintext=plaintext,
+                                signature=signature,
+                                sender_id=sender_id
+                                )
+    @classmethod
+    def get_message(cls, hsh:bytes)->dict :
+        """Raises KeyError is hash not found."""
+        return cls.__messages[hsh]
 
-            cls.__instance = instance
 
-        return cls.__instance
+def new_user(nom="", adresse="", logo="")->int :
+    """Generates keys and all, return the new user's id"""
+    pub, priv = rsa.newkeys(1024)
+    return Server.register_user(nom, adresse, logo, pubkey=pub, privkey=priv)
 
-    def new_user(self)->dict :
-        pub, priv = rsa.newkeys(1024)
-        uid = __class__.__userid
-        __class__.__userid += 1
-        self.register_user(uid, pubkey=pub, privkey=priv)
-        return dict(id=uid, pub=pub, priv=priv)
 
-    def get_pubkey(self, user_id:int) :
-        return self.pubkeys.get(user_id)
 
-    def get_privkey(self, user_id:int) :
-        """Not to be actually used; the private key is _private_, never to be shared → stored locally on the client's device"""
-        return self._privkeys[user_id]
-
-    def register_user(self, user_id:int, *, pubkey, privkey) :
-        if user_id in self.pubkeys :
-            if self.pubkeys[user_id] != pubkey :
-                raise RuntimeError("A user with this id has already registered with a different key.")
-        else :
-            self.pubkeys[user_id] = pubkey
-            self._privkeys[user_id] = privkey
-
-    def register_message(self, hsh) :
-        self.hashes.add(hsh)
-
-    def check_existence(self, hsh) :
-        """Returns True if the hash has been found to be registered."""
-        return hsh in self.hashes
+__all__ = ("new_user", "Message")
 
 if __name__ == '__main__':
-    s = Server()
-    Alice = s.new_user()
-    Bob = s.new_user()
+    Alice = new_user()
+    Bob = new_user()
 
     ## transaction
 
     # premier message sans parent
-    m1 = Message(Alice["id"], "J'ai donné 3 pommes à Bob.".encode())
+    m1 = Message(Alice, "J'ai donné 3 pommes à Bob.".encode())
     auth1 = m1.sign() ; del m1
 
-    # QR code pour transmettre auth1
+    # auth1 passe dans le monde réel :)
 
-    m2 = Message(Bob["id"], "J'ai mangé les trois pommes d'Alice".encode(), Message.from_signature(**auth1))
-
+    m2 = Message(Bob, "J'ai mangé les trois pommes d'Alice".encode(), Message.from_signature(auth1))
     auth2 = m2.sign() ; del m2
 
-    # QR code pour transmettre auth2
-
-    ## récupération
-
-    restored = Message.from_signature(**auth2)
-    msg = pickletools.optimize(pickle.dumps(restored))
-    print(msg, len(msg))
+    # auth2 passe dans le monde réel :)
+    restored = Message.from_signature(auth2)
 
     print(restored.message.decode()) # texte du dernier message
     print(restored.oldmessages[0].message.decode()) # accès au(x) parent(s)
-
-# ----------------------------------------------------------------------------------------------------------------
-#
-# API : { [BDD : id -> blob message chiffré] -> [modèle : blob message chiffré -> objet message clair] -> JSON }
-#
-#
-# BDD:
-# MESSAGES : hash | blob
-# ENTITIES : id | name | pubkey | privkey | adress | …
-#
-# /message/{hash}
-# /message/create/ [POST {{user_id}/{texte}/{parentid1}/{parentid2}/{parentid2}}]
-#
-# /entity/{id}
-# /entity/create/… [POST]
